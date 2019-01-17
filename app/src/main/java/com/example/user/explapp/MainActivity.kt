14f8d738509deb8764a6095cda2d27714375e887
antileha <anti@leha.com>
2019-01-18 00:27:29 +0300

    Added Directory Picker buttot, that start service after choosing

--- app/src/main/java/com/example/user/explapp/MainActivity.kt
+++ app/src/main/java/com/example/user/explapp/MainActivity.kt
@@ -1,10 +1,15 @@
 package com.example.user.explapp
 
+import android.app.Activity
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.content.Intent
+import android.net.Uri
+import android.provider.DocumentsContract
+import android.support.v4.provider.DocumentFile
 import android.widget.EditText
+import java.net.URI
 
 public class MainActivity : AppCompatActivity() {
 
@@ -24,5 +29,22 @@ public class MainActivity : AppCompatActivity() {
         val msg : String = editText.text.toString()
         if(msg != "")
             startService(Intent(this, MyService::class.java).putExtra("dir", rootDir + msg))
+    }
+
+    fun openChooseDir(view : View)
+    {
+        val intent : Intent = Intent(Intent.ACTION_OPEN_DOCUMENT_TREE)
+        startActivityForResult(intent, 42)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(requestCode == 42 && resultCode == Activity.RESULT_OK)
+        {
+            val uri : Uri = data!!.data
+            val path : String = uri.path.split(":")[1] + "/"
+
+            startService(Intent(this, MyService::class.java).putExtra("dir", rootDir + path))
+        }
     }
 }
