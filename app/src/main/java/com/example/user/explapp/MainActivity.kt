2d47a5991d265be82701cd020b264fedab726787
antileha <anti@leha.com>
2019-01-20 16:23:39 +0300

    Now main activity displays all checking directories

--- app/src/main/java/com/example/user/explapp/MainActivity.kt
+++ app/src/main/java/com/example/user/explapp/MainActivity.kt
@@ -2,13 +2,24 @@ package com.example.user.explapp
 
 import android.app.Activity
 import android.support.v7.app.AppCompatActivity
+
 import android.os.Bundle
 import android.view.View
 import android.content.Intent
+
 import android.net.Uri
+
 import android.provider.DocumentsContract
 import android.support.v4.provider.DocumentFile
+import android.util.Log
+
+import android.graphics.Color
+
 import android.widget.EditText
+import android.widget.TextView
+import android.widget.LinearLayout
+import android.widget.LinearLayout.LayoutParams
+
 import java.net.URI
 
 public class MainActivity : AppCompatActivity() {
@@ -17,10 +28,13 @@ public class MainActivity : AppCompatActivity() {
 
     private val rootDir : String = "/storage/emulated/0/"
 
+    private lateinit var dirsScrollLayout : LinearLayout
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_main)
+        dirsScrollLayout = findViewById(R.id.dirsLayout) as LinearLayout
     }
 
     fun sendAnsewer(view : View)
@@ -43,6 +57,21 @@ public class MainActivity : AppCompatActivity() {
         {
             val uri : Uri = data!!.data
             val path : String = uri.path.split(":")[1] + "/"
+
+            val newDirView : TextView = TextView(this)
+
+            val layoutParams : LayoutParams = LayoutParams(LayoutParams.MATCH_PARENT,
+                    LayoutParams.MATCH_PARENT, 1.0f)
+            layoutParams.setMargins(10, 10, 10, 10)
+
+            newDirView.layoutParams = layoutParams
+            newDirView.text = path
+            newDirView.textSize = 18.0f
+            newDirView.setBackgroundColor(Color.parseColor("#bdbdbd"))
+
+
+            dirsScrollLayout.addView(newDirView)
+            dirsScrollLayout.invalidate()
 
             startService(Intent(this, MyService::class.java).putExtra("dir", rootDir + path))
         }
